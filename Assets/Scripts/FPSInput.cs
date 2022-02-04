f6cfca09fba7e071d7926df69bdd0c8663b855e8
Evgenii Morozov <smailus2@gmail.com>
2022-02-04 13:10:59 +0300

    Добавьте файлы проекта.

---
+++ Assets/Scripts/FPSInput.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float gravity = -9.8f;
+
+    private CharacterController _charController;
+
+    private void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+    }
+}
